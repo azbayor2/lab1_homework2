d62d59ce6e72a7b83dc39e49323752e3a2fe9731
azbayor2 <hanharry2@gmail.com>
2025-10-27 09:58:57 +0900

    added userBuilder

--- Library/user/User.java
+++ Library/user/User.java
@@ -1,6 +1,6 @@
 package Library.User;
 
-class User {
+public class User {
     protected String Name;
     protected String Birthdate;
     protected String Gender;
@@ -8,44 +8,3 @@ class User {
     protected String Address;
     protected String PrivateID;
 }
-
-class UserBuilder{
-    protected String Name="";
-    protected String Birthdate="";
-    protected String Gender="";
-    protected String PhoneNumber="";
-    protected String Address="";
-    protected String PrivateID="";
-
-    public UserBuilder name(String name){
-        this.Name = name;
-        return this;
-    }
-
-    public UserBuilder birthdate(String birthdate){
-        this.Birthdate = birthdate;
-        return this;
-    }
-
-    public UserBuilder gender(String gender){
-        this.Gender = gender;
-        return this;
-    }
-
-    public UserBuilder phonenumber(String phonenumber){
-        this.PhoneNumber = phonenumber;
-        return this;
-    }
-
-    public UserBuilder address(String address){
-        this.Address = address;
-        return this;
-    }
-
-    public UserBuilder privateid(String privateid){
-        this.PrivateID = privateid;
-        return this;
-    }
-}
-
-
